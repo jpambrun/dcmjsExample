a867d068344a0f2983ecb3ad1baa2fb89904965a
Jean-Francois Pambrun <jf.pambrun@gmail.com>
2017-09-26 10:06:13 -0400

    wip

--- index.js
+++ index.js
@@ -9,6 +9,112 @@ function readDICOMFile(filename) {
   return dataset;
 }
 
+
+function createImageFrame(pixelBuffer, pixelMetaData, data){
+  const now = new Date();
+  const currentDateString = `${now.getFullYear()}` + 
+                            `${now.getMonth().toString().padStart(2,'0')}` +
+                            `${now.getDate().toString().padStart(2,'0')}`;
+  const currentTimeString = `${now.getHours().toString().padStart(2,'0')}` + 
+                            `${now.getMinutes().toString().padStart(2,'0')}` + 
+                            `${now.getSeconds().toString().padStart(2,'0')}`;
+
+  const dataset = {
+    ImageType: [ 'ORIGINAL', 'PRIMARY', 'AXIAL' ],
+    InstanceCreationDate: currentDateString,
+    InstanceCreationTime: currentTimeString,
+    SOPClassUID: '1.2.840.10008.5.1.4.1.1.2',
+    SOPInstanceUID: DCMJS.data.DicomMetaDictionary.uid(),
+    StudyDate: currentDateString,
+    SeriesDate: currentDateString,
+    AcquisitionDate: currentDateString,
+    ContentDate: currentDateString,
+    StudyTime: currentTimeString,
+    SeriesTime: currentTimeString,
+    AcquisitionTime: currentTimeString,
+    ContentTime: currentTimeString,
+    AccessionNumber: 'AN' + Math.floor(Math.random()*100000000),
+    Modality: 'CT',
+    Manufacturer: 'None',
+    InstitutionName: 'DefaultInstitutionName',
+    ReferringPhysicianName: '',
+    StudyDescription: 'DefaultDefaultInstitutionName',
+    ManufacturerModelName: 'DefaultManufacturerModelName',
+    PatientName: 'DefaultPN',
+    PatientID: 'DefaultPID',
+    PatientBirthDate: '19000909',
+    PatientSex: '117',
+    PatientAge: 'M',
+    BodyPartExamined: 'CHEST',
+    ScanOptions: 'HELICAL MODE',
+    SliceThickness: '0.625000',
+    KVP: '120',
+    DataCollectionDiameter: '500.000000',
+    SoftwareVersions: '06MW03.4',
+    ReconstructionDiameter: '360.000000',
+    DistanceSourceToDetector: '949.075012',
+    DistanceSourceToPatient: '541.000000',
+    GantryDetectorTilt: '0.000000',
+    TableHeight: '154.000000',
+    RotationDirection: 'CW',
+    ExposureTime: '500',
+    XRayTubeCurrent: '577',
+    Exposure: '3',
+    FilterType: 'BODY FILTER',
+    GeneratorPower: '90000',
+    FocalSpots: '1.200000',
+    ConvolutionKernel: 'STANDARD',
+    PatientPosition: 'FFS',
+    StudyInstanceUID: DCMJS.data.DicomMetaDictionary.uid(),
+    SeriesInstanceUID: DCMJS.data.DicomMetaDictionary.uid(),
+    StudyID: '',
+    SeriesNumber: '3',
+    AcquisitionNumber: '1',
+    InstanceNumber: '1',
+    ImagePositionPatient: [ '0.0000', '0.000', '0.00000' ],
+    ImageOrientationPatient: 
+     [ '1.000000',
+       '0.000000',
+       '0.000000',
+       '0.000000',
+       '1.000000',
+       '0.000000' ],
+    FrameOfReferenceUID: DCMJS.data.DicomMetaDictionary.uid(),
+    PositionReferenceIndicator: 'SN',
+    SliceLocation: '15.000000',
+    SamplesPerPixel: 1,
+    PhotometricInterpretation: 'MONOCHROME2',
+    Rows: pixelMetaData.Rows,
+    Columns: pixelMetaData.Columns,
+    PixelSpacing: [ '0.703125', '0.703125' ],
+    BitsAllocated: pixelMetaData.BitsAllocated,
+    BitsStored: pixelMetaData.BitsStored,
+    HighBit: pixelMetaData.HighBit,
+    PixelRepresentation: pixelMetaData.PixelRepresentation,
+    PixelPaddingValue: pixelMetaData.PixelPaddingValue,
+    WindowCenter: '100',
+    WindowWidth: '750',
+    RescaleIntercept: pixelMetaData.RescaleIntercept.toString(10),
+    RescaleSlope: pixelMetaData.RescaleSlope.toString(10),
+    RescaleType: 'HU',
+    PerformedProcedureStepStartDate: currentDateString,
+    PerformedProcedureStepStartTime: currentTimeString,
+    RequestedProcedureID: '',
+    PixelData: pixelBuffer,
+    _meta: {
+      FileMetaInformationVersion:{ vr: 'OB', Value: [(new Uint8Array([0, 1])).buffer]}
+    },
+    _vrMap: { PixelData: 'OW' },
+  };
+
+  // override properties of dataset with those of data.
+  Object.assign(dataset, data);
+  console.log(data.PatientName)
+  console.log(dataset.PatientName)
+
+  return dataset;
+}
+
 function writeDICOMFile(filename, dataset) {
   const EXPLICIT_LITTLE_ENDIAN = '1.2.840.10008.1.2.1';
   const meta = {
@@ -29,107 +135,46 @@ function writeDICOMFile(filename, dataset) {
 
 
 
-const dataset = {
-  ImageType: [ 'ORIGINAL', 'PRIMARY', 'AXIAL' ],
-  InstanceCreationDate: '20060629',
-  InstanceCreationTime: '125429',
-  SOPClassUID: '1.2.840.10008.5.1.4.1.1.2',
-  SOPInstanceUID: '1.3.6.1.4.1.9328.50.17.156813157469317477610145582477245987122',
-  StudyDate: '20060629',
-  SeriesDate: '20060629',
-  AcquisitionDate: '20060629',
-  ContentDate: '20060629',
-  StudyTime: '124722',
-  SeriesTime: '125233',
-  AcquisitionTime: '125420',
-  ContentTime: '125429',
-  AccessionNumber: '',
-  Modality: 'CT',
-  Manufacturer: 'GE MEDICAL SYSTEMS',
-  InstitutionName: '',
-  ReferringPhysicianName: '',
-  StudyDescription: 'Unspecified CT',
-  ManufacturerModelName: 'LightSpeed VCT',
-  PatientName: '157631',
-  PatientID: 'RIDER-1006487867',
-  PatientBirthDate: '',
-  PatientSex: '',
-  PatientAge: '',
-  PatientIdentityRemoved: 'YES',
-  DeidentificationMethod: 'CTP: DICOM-S142-Baseline: 20090420:133941',
-  '00130010': 'CTP',
-  '00131010': 'RIDER Lung PET-CT\u0000',
-  '00131013': '56713007',
-  BodyPartExamined: 'CHEST',
-  ScanOptions: 'HELICAL MODE',
-  SliceThickness: '0.625000',
-  KVP: '120',
-  DataCollectionDiameter: '500.000000',
-  SoftwareVersions: '06MW03.4',
-  ReconstructionDiameter: '360.000000',
-  DistanceSourceToDetector: '949.075012',
-  DistanceSourceToPatient: '541.000000',
-  GantryDetectorTilt: '0.000000',
-  TableHeight: '154.000000',
-  RotationDirection: 'CW',
-  ExposureTime: '500',
-  XRayTubeCurrent: '577',
-  Exposure: '3',
-  FilterType: 'BODY FILTER',
-  GeneratorPower: '90000',
-  FocalSpots: '1.200000',
-  ConvolutionKernel: 'STANDARD',
-  PatientPosition: 'FFS',
-  StudyInstanceUID: '1.3.6.1.4.1.9328.50.17.106165719864837115866539427306648068553',
-  SeriesInstanceUID: '1.3.6.1.4.1.9328.50.17.249836005616032823097408901947312961772',
-  StudyID: '',
-  SeriesNumber: '3',
-  AcquisitionNumber: '1',
-  InstanceNumber: '1',
-  ImagePositionPatient: [ '-174.000000', '-181.000000', '15.000000' ],
-  ImageOrientationPatient: 
-   [ '1.000000',
-     '0.000000',
-     '0.000000',
-     '0.000000',
-     '1.000000',
-     '0.000000' ],
-  FrameOfReferenceUID: '1.3.6.1.4.1.9328.50.17.332813002545792437845894102858881370357',
-  PositionReferenceIndicator: 'SN',
-  SliceLocation: '15.000000',
-  SamplesPerPixel: 1,
-  PhotometricInterpretation: 'MONOCHROME2',
+// #########################
+
+const now = new Date();
+const currentDateString = `${now.getFullYear()}` + 
+`${now.getMonth().toString().padStart(2,'0')}` +
+`${now.getDate().toString().padStart(2,'0')}`;
+const currentTimeString = `${now.getHours().toString().padStart(2,'0')}` + 
+`${now.getMinutes().toString().padStart(2,'0')}` + 
+`${now.getSeconds().toString().padStart(2,'0')}`;
+
+const pixelMetaData = {
   Rows: 512,
   Columns: 512,
-  PixelSpacing: [ '0.703125', '0.703125' ],
   BitsAllocated: 16,
   BitsStored: 16,
   HighBit: 15,
   PixelRepresentation: 1,
   PixelPaddingValue: -2000,
-  WindowCenter: '100',
-  WindowWidth: '750',
-  RescaleIntercept: '-1024',
-  RescaleSlope: '1',
-  RescaleType: 'HU',
-  PerformedProcedureStepStartDate: '20060629',
-  PerformedProcedureStepStartTime: '124722',
-  RequestedProcedureID: '',
-  PixelData: (new Int16Array(512*512)).buffer,
-  _meta: {
-    FileMetaInformationVersion:{ vr: 'OB', Value: [(new Uint8Array([0, 1])).buffer]}
-  },
-  _vrMap: { PixelData: 'OW' },
+  RescaleIntercept: -1000,
+  RescaleSlope: 1,
 };
 
-// data.
+const data = {
+  PatientName : 'MR PAMBRUN',
+  PatientID : 'PID' + Math.floor(Math.random()*100000000),
+  AccessionNumber: 'AN' + Math.floor(Math.random()*100000000),
+  StudyDescription: 'Test Study',
+  SeriesNumber: '3',
+  InstanceNumber: '1',
+  StudyInstanceUID: DCMJS.data.DicomMetaDictionary.uid(),
+  SeriesInstanceUID: DCMJS.data.DicomMetaDictionary.uid(),
+  FrameOfReferenceUID: DCMJS.data.DicomMetaDictionary.uid(),
+}
 
 
-// const dataset1 = readDICOMFile('000001.dcm');
-// console.log(dataset._meta.FileMetaInformationVersion.Value)
-// console.log(dataset1._meta.FileMetaInformationVersion.Value)
-// console.log(dataset)
-dataset.PatientName = 'Mr Pambrun';
-writeDICOMFile('out.dcm', dataset);
-
-
+for (let i = 0; i < 100; i++) {
+  const pixelData = new Int16Array(515*512);
+  pixelData.fill(i * 10);
+  data.InstanceNumber = (i+1).toString();
+  ImagePositionPatient: [ '0.0000', '0.0000', i.toString() ],
+  dataset = createImageFrame(pixelData.buffer, pixelMetaData, data);
+  writeDICOMFile(`out${i.toString().padStart('0', 6)}.dcm`, dataset);  
+}
